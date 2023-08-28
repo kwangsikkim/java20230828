3547da41ca7019a0c0121c6587f8d543f83f3e5a
kwangsikkim <penggusic@gmail.com>
2023-08-28 11:43:42 +0900

    first

---
+++ src/ch05reference/C01primitive.java
@@ -0,0 +1,14 @@
+package ch05reference;
+
+public class C01primitive {
+	public static void main(String[] args) {
+		int a; //  변수 선언
+		a = 3; // 데이터 타입(자료형)에 맞는 값만 대입 가능
+ 	//	a = 3.14 // 데이터 타입에 맞지 않으면 대입 불가능
+	
+		int b =5; // 선언과 동시에 값 대입 가능
+		
+	
+	
+	}	
+}
