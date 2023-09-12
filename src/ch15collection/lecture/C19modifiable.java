bc56990d01892ed75e0113397e642bbab823c5d4
kwangsikkim <penggusic@gmail.com>
2023-09-12 11:54:25 +0900

    modifiable

---
+++ src/ch15collection/lecture/C19modifiable.java
@@ -0,0 +1,20 @@
+package ch15collection.lecture;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C19modifiable {
+    public static void main(String[] args) {
+        List<String> list1 = List.of("html", "css", "react", "js");
+//        list1.add("java");    // 안됨
+
+        List<String > list2 = new ArrayList<>(list1);
+        list2.add("java");
+
+        System.out.println("list1.size() = " + list1.size());
+        System.out.println("list2.size() = " + list2.size());
+
+
+        System.out.println("프로그램 실행 중..");
+    }
+}
